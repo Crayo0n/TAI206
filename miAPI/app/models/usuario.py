5f86f8d4e1fd7c33a120bffc41415735762749fa
Crayo0n <mauricio28rd@gmail.com>
2026-03-25 09:35:10 -0600

    Endpoitns trabajando con la BD

--- miAPI/app/models/usuario.py
+++ miAPI/app/models/usuario.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 #Modelo de validacion Pydantic
 class UsuarioBase(BaseModel):
-    nombre: str = Field(..., min_length=3, max_length=50, description="Nombre del usuario")
-    edad: int = Field(..., ge=0, le=121, description="Edad valida entre 0 y 121")
+    nombre: Optional[str] = Field(..., min_length=3, max_length=50, description="Nombre del usuario")
+    edad: Optional [int] = Field(..., ge=0, le=121, description="Edad valida entre 0 y 121")
     
--- miAPI/app/routers/usuarios.py
+++ miAPI/app/routers/usuarios.py
@@ -12,6 +12,7 @@ router= APIRouter(
     tags= ["CRUD Usuarios"]
 )
 
+#Consulta todos Usuarios
 @router.get("/")
 async def consultaUsuarios(db: Session = Depends(get_db)):
     consulta_usuarios = db.query(usuarioDB).all()
@@ -20,7 +21,17 @@ async def consultaUsuarios(db: Session = Depends(get_db)):
         "total": len(consulta_usuarios),
         "data": consulta_usuarios
     }
-    
+
+#Consulta un usuario
+@router.get("/{id}")
+async def consultaUsuario_ID(id: int, db: Session = Depends(get_db)):
+    usuario_ID = db.query(usuarioDB).filter(usuarioDB.id == id).first()
+    return {
+        "status": "200",
+        "data": usuario_ID
+    }   
+
+#Agregar un usuario
 @router.post("/")
 async def agregar_Usuario(usuario:UsuarioBase, db: Session = Depends(get_db)):
     
@@ -35,31 +46,48 @@ async def agregar_Usuario(usuario:UsuarioBase, db: Session = Depends(get_db)):
         "status": "200"
     }
 
+#Editar Usuario
 @router.put("/{id}", status_code=status.HTTP_200_OK)
-async def editar_Usuario(id:int, usuario:dict):
-    for i, usr in enumerate(usuarios):
-        if usr["id"]==usuario.get("id"):
-            usuarios[i] = usuario
-            return{
-                "mensaje":"Usuario Editado",
-                "datos":usuario,
-                "status": "200"
-            }
-    raise HTTPException(
-        status_code=400,
-        detail="Usuario no encontrado"
-    )
+async def editar_Usuario(id: int, usuario: UsuarioBase, db: Session = Depends(get_db)):
+    usuario_existente = db.query(usuarioDB).filter(usuarioDB.id == id).first()
     
+    usuario_existente.nombre = usuario.nombre
+    usuario_existente.edad = usuario.edad
+    db.commit()
+    db.refresh(usuario_existente)
+    return {
+        "mensaje": "Usuario Editado",
+        "datos": usuario_existente
+    }
+
+
+#Editar Usuario (Patch)
+@router.patch("/{id}", status_code=status.HTTP_200_OK)
+async def editarU_patch(id: int, usuario: UsuarioBase, db: Session = Depends(get_db)):
+    editaUsuario = db.query(usuarioDB).filter(usuarioDB.id == id).first()
+    
+    if usuario.nombre is not None:
+        editaUsuario.nombre = usuario.nombre
+    if usuario.edad is not None:
+        editaUsuario.edad = usuario.edad
+    
+    db.commit()
+    db.refresh(editaUsuario)
+    
+    return {
+        "mensaje": "Usuario Editado",
+        "datos": editaUsuario,
+        "status": "200"
+    }
+
+#Eliminar Usuario
 @router.delete("/{id}")
-async def eliminar_Usuario(id:int, usuarioAuth: str = Depends(verificar_Peticion)):
-    for i, usr in enumerate(usuarios):
-        if usr["id"]==id:
-            usuarios.pop(i)
-            return{
-                "mensaje":f"Usuario Eliminado correctamente por {usuarioAuth}",
-                "status": "200"
-            }
-    raise HTTPException(
-        status_code=400,
-        detail="Usuario no encontrado"
-    )
+async def eliminar_Usuario(id:int, db: Session= Depends(get_db), usuarioAuth: str = Depends(verificar_Peticion)):
+    usuario_ID = db.query(usuarioDB).filter(usuarioDB.id == id).first()
+    db.delete(usuario_ID)
+    db.commit()
+    return {
+        "mensaje": f"Usuario Eliminado correctamente por {usuarioAuth}",
+        "status": "200"
+    }
+
